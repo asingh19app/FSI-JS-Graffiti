758570d8d3113fde2e3734960605325041517742
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-06-11 18:01:15 -0400

    added js

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Select some elements...
+
+let myHeader = document.querySelector ('#page-header')
+myHeader.style.textAlign = 'left'
+
+
+let dogImages =  document.querySelectorAll ('.dog-image')
+for (let i = 0; i< dogImages.length; i ++) {
+
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.rotate = '180deg'
+}
+
+
+let dogNames = document.querySelectorAll ('.dog-name')
+
+for ( let i = 0; i <dogNames.length; i++) {
+
+    dogNames[i].style.textAlign = 'left'
+}
+
+let dogDes = document.querySelectorAll ('.dog-description')
+
+for ( let i = 0; i < dogDes.length; i++) {
+
+    dogDes[i].style.color = 'pink'
+    
+}
+
